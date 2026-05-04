b58d2e0a61a92e761412ebb41819f556fe34b4c2
ABHAY SONI <94478173+ABHAY0301@users.noreply.github.com>
2026-05-04 09:12:46 +0530

    Update Day9.sql

--- Day9.sql
+++ Day9.sql
@@ -1,6 +1,7 @@
 /*
--- Question Day-09/30 Find Department's Top three 
-salaries in each department.	
+-- Question Day-09/30 
+
+Find Department's Top three salaries in each department.	
 
 A company's executives are interested in seeing who earns the most money in each of the company's departments. 
 A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
